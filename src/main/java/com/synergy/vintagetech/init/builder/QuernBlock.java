5646371b5e335369cd15335a3f7d8937ebf742c3
DevDyna <andrearisorti2003@gmail.com>
2026-05-24 20:34:48 +0200

    Update QuernBlock.java

--- src/main/java/com/synergy/vintagetech/init/builder/QuernBlock.java
+++ src/main/java/com/synergy/vintagetech/init/builder/QuernBlock.java
@@ -2,14 +2,31 @@ package com.synergy.vintagetech.init.builder;
 
 import java.util.List;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class QuernBlock extends MonoDirectionalAxleBlock {
 
     public QuernBlock(Properties p) {
         super(p);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.join(
+                Shapes.join(axleY,
+                        Shapes.box(0.0625, 0, 0.0625, 0.9375, 0.25, 0.9375),
+                        BooleanOp.OR),
+                Shapes.box(0.125, 0.25, 0.125, 0.875, 0.5, 0.875),
+                BooleanOp.OR)
+                .optimize();
     }
 
     @Override
