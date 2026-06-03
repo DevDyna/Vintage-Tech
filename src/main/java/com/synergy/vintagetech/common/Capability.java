058e5744ce241b93ddc5e8c5637eb92b146d0c23
DevDyna <andrearisorti2003@gmail.com>
2026-06-04 01:45:18 +0200

    Update Capability.java

--- src/main/java/com/synergy/vintagetech/common/Capability.java
+++ src/main/java/com/synergy/vintagetech/common/Capability.java
@@ -1,8 +1,5 @@
 package com.synergy.vintagetech.common;
 
-import com.devdyna.cakesticklib.api.CapabilityUtils;
-import com.synergy.vintagetech.init.types.zBlocks;
-
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
@@ -10,6 +7,6 @@ public class Capability {
 
     @SubscribeEvent
     public static void register(RegisterCapabilitiesEvent event) {
-        CapabilityUtils.registerFluidBlocks(event, zBlocks.ENGINE.get());
+        // CapabilityUtils.registerFluidBlocks(event, zBlocks.ENGINE.get());
     }
 }
