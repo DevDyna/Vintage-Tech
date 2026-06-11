495a0b2f2642c61a3ddc8ba6e7fe60b3b20a9186
DevDyna <andrearisorti2003@gmail.com>
2026-06-11 17:17:33 +0200

    fixed basket voxelshape not work correctly

--- src/main/java/com/synergy/vintagetech/init/builder/hopper/BasketBlock.java
+++ src/main/java/com/synergy/vintagetech/init/builder/hopper/BasketBlock.java
@@ -59,11 +59,11 @@ public class BasketBlock extends TickingBlock {
                 shape = Shapes.join(shape, Shapes.box(0, 0, 0, 0.75, 1, 0.1875), BooleanOp.OR);
             }
             case WEST -> {
-                shape = Shapes.join(shape, Shapes.box(0, 0, 0, 1, 1, 0.25), BooleanOp.OR);
-                shape = Shapes.join(shape, Shapes.box(0.1875, 0.8125, 0.25, 0.8125, 1, 1), BooleanOp.OR);
-                shape = Shapes.join(shape, Shapes.box(0.1875, 0, 0.25, 0.8125, 0.1875, 1), BooleanOp.OR);
-                shape = Shapes.join(shape, Shapes.box(0.8125, 0, 0.25, 1, 1, 1), BooleanOp.OR);
-                shape = Shapes.join(shape, Shapes.box(0, 0, 0.25, 0.1875, 1, 1), BooleanOp.OR);
+                shape = Shapes.join(shape, Shapes.box(0.75, 0, 0, 1, 1, 1), BooleanOp.OR);
+                shape = Shapes.join(shape, Shapes.box(0, 0.1875, 0.8125, 0.75, 0.8125, 1), BooleanOp.OR);
+                shape = Shapes.join(shape, Shapes.box(0, 0.1875, 0, 0.75, 0.8125, 0.1875), BooleanOp.OR);
+                shape = Shapes.join(shape, Shapes.box(0, 0, 0, 0.75, 0.1875, 1), BooleanOp.OR);
+                shape = Shapes.join(shape, Shapes.box(0, 0.8125, 0, 0.75, 1, 1), BooleanOp.OR);
             }
             case EAST -> {
                 shape = Shapes.join(shape, Shapes.box(0, 0, 0, 0.25, 1, 1), BooleanOp.OR);
