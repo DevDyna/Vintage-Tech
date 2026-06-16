0878f31eee35010d33b8f7e99c8882a0a2c68bc1
DevDyna <andrearisorti2003@gmail.com>
2026-06-16 14:21:10 +0200

    fixed steam engine not work at all

--- src/main/java/com/synergy/vintagetech/init/builder/engine/SteamEngineBE.java
+++ src/main/java/com/synergy/vintagetech/init/builder/engine/SteamEngineBE.java
@@ -1,16 +1,15 @@
 package com.synergy.vintagetech.init.builder.engine;
 
-import com.synergy.vintagetech.api.blockfactory.KineticGenerator;
-import com.synergy.vintagetech.api.blockfactory.transmission.TransmissionBE;
+import com.synergy.vintagetech.api.blockfactory.engine.BaseEngineBE;
 import com.synergy.vintagetech.init.types.zBlockEntities;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class SteamEngineBE extends TransmissionBE implements KineticGenerator {
+public class SteamEngineBE extends BaseEngineBE {
 
     public SteamEngineBE(BlockPos p, BlockState s) {
         super(zBlockEntities.STEAM_ENGINE.get(), p, s);
     }
-
+    //TODO IMP : rework
 }
