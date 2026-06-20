380590b26dc86f0fa15c3a370ea35bf0e54feb54
DevDyna <andrearisorti2003@gmail.com>
2026-06-20 22:01:16 +0200

    Update RecipeSender.java

--- src/main/java/com/synergy/vintagetech/common/RecipeSender.java
+++ src/main/java/com/synergy/vintagetech/common/RecipeSender.java
@@ -1,6 +1,7 @@
 package com.synergy.vintagetech.common;
 
 import com.devdyna.cakesticklib.api.utils.ModAddonUtil;
+import com.synergy.vintagetech.init.types.zRecipeTypes;
 
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.OnDatapackSyncEvent;
@@ -9,7 +10,11 @@ public class RecipeSender {
     @SubscribeEvent
     public static void onDatapackSync(OnDatapackSyncEvent event) {
         if (ModAddonUtil.checkMod("jei")) {
-
+            event.sendRecipes(
+                    zRecipeTypes.CRUSHING_TUB.getType(),
+                    zRecipeTypes.DRYING_RACK.getType(),
+                    zRecipeTypes.EVAPORATION_BASIN.getType(),
+                    zRecipeTypes.MILLSTONE.getType());
         }
 
     }
