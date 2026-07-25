23ad0c0f6ab70de9174c6e769fd8c71558674ca8
DevDyna <andrearisorti2003@gmail.com>
2026-07-25 13:41:11 +0200

    Update BlockModelUtils.java

--- src/main/java/com/synergy/vintagetech/api/BlockModelUtils.java
+++ src/main/java/com/synergy/vintagetech/api/BlockModelUtils.java
@@ -160,7 +160,7 @@ public class BlockModelUtils {
                                                 Optional.of(x.rl(MODULE_ID, "block/template/beam")), Optional.empty(),
                                                 TextureSlot.TOP, TextureSlot.SIDE)
                                                 .create(
-                                                                x.rl(MODULE_ID, "block/beam/" + x.name(block)
+                                                                x.rl(MODULE_ID, "block/" + x.name(block)
                                                                                 .replace("_beam", "")),
                                                                 new TextureMapping()
                                                                                 .put(
