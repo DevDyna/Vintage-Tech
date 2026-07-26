d7078dba0789cd1957cefae3f8386156aed703dc
DevDyna <andrearisorti2003@gmail.com>
2026-07-27 01:15:21 +0200

    init item tooltipped event

--- src/main/java/com/synergy/vintagetech/GameEvents.java
+++ src/main/java/com/synergy/vintagetech/GameEvents.java
@@ -13,6 +13,7 @@ public class GameEvents {
         bus.addListener(Capability::register);
         bus.register(CreativeTabs.class);
         NeoForge.EVENT_BUS.register(RecipeSender.class);
+        NeoForge.EVENT_BUS.register(ItemToolTipped.class);
         bus.addListener(FlowerPotRegistry::common);
 
     }
