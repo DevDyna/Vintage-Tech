85880133cc203498f1f88db66dd60bd1ef3d98e1
DevDyna <andrearisorti2003@gmail.com>
2026-07-27 01:15:49 +0200

    added strippable to ironwood blocks

--- src/main/java/com/synergy/vintagetech/datagen/server/DataMaps.java
+++ src/main/java/com/synergy/vintagetech/datagen/server/DataMaps.java
@@ -2,9 +2,14 @@ package com.synergy.vintagetech.datagen.server;
 
 import java.util.concurrent.CompletableFuture;
 
+import com.synergy.vintagetech.init.types.zBlocks;
+
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.conditions.AlwaysCondition;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+import net.neoforged.neoforge.registries.datamaps.builtin.Strippable;
 
 public class DataMaps extends DataMapProvider {
 
@@ -14,6 +19,16 @@ public class DataMaps extends DataMapProvider {
 
         @Override
         protected void gather(Provider p) {
+
+                builder(NeoForgeDataMaps.STRIPPABLES)
+                                .add(zBlocks.IRONWOOD_LOG.getKey(),
+                                                new Strippable(zBlocks.STRIPPED_IRONWOOD_LOG.get()),
+                                                false, AlwaysCondition.INSTANCE);
+
+                builder(NeoForgeDataMaps.STRIPPABLES)
+                                .add(zBlocks.IRONWOOD_WOOD.getKey(),
+                                                new Strippable(zBlocks.STRIPPED_IRONWOOD_WOOD.get()),
+                                                false, AlwaysCondition.INSTANCE);
 
         }
 
