948c7c5f0a66c44b6ce14bd786e4511f805bb58e
DevDyna <andrearisorti2003@gmail.com>
2026-07-28 17:26:01 +0200

    removed salt solution due pointless

--- src/main/java/com/synergy/vintagetech/init/types/zFluids.java
+++ src/main/java/com/synergy/vintagetech/init/types/zFluids.java
@@ -24,8 +24,7 @@ public class zFluids {
         public static final DeferredRegister<FluidType> zFluidTypes = DeferredRegister.create(Keys.FLUID_TYPES,
                         MODULE_ID);
 
-        public static final FluidRegister SALT_SOLUTION = FluidRegister.create("salt_solution",
-                        ColorUtils.argb(255, 236, 202, 202));
+        
         public static final FluidRegister SOY_WHEY = FluidRegister.create("soy_whey",
                         ColorUtils.argb(255, 255, 236, 236));
 
