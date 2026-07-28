c38111b88e7e61e6be6cfb362174ada9aa66f152
DevDyna <andrearisorti2003@gmail.com>
2026-07-28 17:19:59 +0200

    Update AirFlowParticle.java

--- src/main/java/com/synergy/vintagetech/client/particles/fan/AirFlowParticle.java
+++ src/main/java/com/synergy/vintagetech/client/particles/fan/AirFlowParticle.java
@@ -10,6 +10,8 @@ public class AirFlowParticle extends SimpleAnimatedParticle {
 
     private final Vec3 dir;
 
+    //TODO BUG : particles stuck on valid empty blocks
+
     public AirFlowParticle(ClientLevel l,
             double x, double y, double z,
             SpriteSet sprites, Vec3 vec3, RandomSource r) {
