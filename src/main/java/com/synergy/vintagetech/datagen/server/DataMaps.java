01cd48241e55ca65cd935a187c55e4ff4b0ac6e4
DevDyna <andrearisorti2003@gmail.com>
2026-07-28 17:44:54 +0200

    updated datamaps and added new compostable and furnace fuels entries

--- src/main/java/com/synergy/vintagetech/datagen/server/DataMaps.java
+++ src/main/java/com/synergy/vintagetech/datagen/server/DataMaps.java
@@ -3,11 +3,15 @@ package com.synergy.vintagetech.datagen.server;
 import java.util.concurrent.CompletableFuture;
 
 import com.synergy.vintagetech.init.types.zBlocks;
+import com.synergy.vintagetech.init.types.zItems;
 
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.neoforged.neoforge.common.conditions.AlwaysCondition;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
 import net.neoforged.neoforge.registries.datamaps.builtin.Strippable;
 
@@ -21,13 +25,58 @@ public class DataMaps extends DataMapProvider {
         protected void gather(Provider p) {
 
                 builder(NeoForgeDataMaps.STRIPPABLES)
-                                .add(zBlocks.IRONWOOD_LOG.getKey(),
+                                .add(zBlocks.IRONWOOD_LOG.getId(),
                                                 new Strippable(zBlocks.STRIPPED_IRONWOOD_LOG.get()),
+                                                false, AlwaysCondition.INSTANCE)
+
+                                .add(zBlocks.IRONWOOD_WOOD.getId(),
+                                                new Strippable(zBlocks.STRIPPED_IRONWOOD_WOOD.get()),
                                                 false, AlwaysCondition.INSTANCE);
 
-                builder(NeoForgeDataMaps.STRIPPABLES)
-                                .add(zBlocks.IRONWOOD_WOOD.getKey(),
-                                                new Strippable(zBlocks.STRIPPED_IRONWOOD_WOOD.get()),
+                builder(NeoForgeDataMaps.COMPOSTABLES)
+                                .add(zBlocks.IRONWOOD_LEAVES.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zBlocks.IRONWOOD_SAPLING.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.ALOE.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.BLUEBERRIES.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zBlocks.LAVENDER.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.HEMP.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.HEMP_SEEDS.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.CAVE_WHEAT_SEEDS.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.IRONBERRIES.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.SOYBEANS.getId(),
+                                                new Compostable(0.3F, false),
+                                                false, AlwaysCondition.INSTANCE);
+
+                builder(NeoForgeDataMaps.FURNACE_FUELS)
+                                .add(zItems.GLUE.getId(),
+                                                new FurnaceFuel(AbstractFurnaceBlockEntity.BURN_TIME_STANDARD * 2),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.BITUMEN.getId(),
+                                                new FurnaceFuel(AbstractFurnaceBlockEntity.BURN_TIME_STANDARD * 4),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zBlocks.CRUSHING_TUB.getId(),
+                                                new FurnaceFuel(AbstractFurnaceBlockEntity.BURN_TIME_STANDARD * 2),
+                                                false, AlwaysCondition.INSTANCE)
+                                .add(zItems.MESH.getId(),
+                                                new FurnaceFuel(AbstractFurnaceBlockEntity.BURN_TIME_STANDARD / 2),
                                                 false, AlwaysCondition.INSTANCE);
 
         }
