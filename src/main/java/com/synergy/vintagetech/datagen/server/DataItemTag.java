5f16053580060d360347ea69686ef8c6d8219d75
DevDyna <andrearisorti2003@gmail.com>
2026-07-29 12:17:42 +0200

    Update DataItemTag.java

--- src/main/java/com/synergy/vintagetech/datagen/server/DataItemTag.java
+++ src/main/java/com/synergy/vintagetech/datagen/server/DataItemTag.java
@@ -121,7 +121,6 @@ public class DataItemTag extends ItemTagsProvider {
                                                 zItems.IRONBERRIES.get());
 
                 tag(Tags.Items.SLIME_BALLS).add(zItems.GLUE.get());
-                // tag(Tags.Items.FOODS).add(zItems.BLUEBERRIES_SOUP.get());
 
                 tag(Tags.Items.FOODS_RAW_MEAT).add(zItems.TOFU.get());
                 tag(Tags.Items.FOODS_RAW_FISH).add(zItems.TOFU.get());
