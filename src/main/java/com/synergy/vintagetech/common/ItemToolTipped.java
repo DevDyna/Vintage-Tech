836779e1ea3d846bfe414750116d26188735425b
DevDyna <andrearisorti2003@gmail.com>
2026-07-31 17:26:58 +0200

    datagen

--- src/main/java/com/synergy/vintagetech/common/ItemToolTipped.java
+++ src/main/java/com/synergy/vintagetech/common/ItemToolTipped.java
@@ -1,42 +1,60 @@
 package com.synergy.vintagetech.common;
 
+import static com.synergy.vintagetech.Main.MODULE_ID;
+
+import com.synergy.vintagetech.init.types.zBlocks;
+import com.synergy.vintagetech.init.types.zItems;
+import com.synergy.vintagetech.init.types.zTags;
+
+import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 public class ItemToolTipped {
-    // private static final int OVER_THE_REGISTRY_ID = 1;
+    private static final int OVER_THE_REGISTRY_ID = 1;
 
     //TODO IMP : tooltips
 
     @SubscribeEvent
     public static void main(ItemTooltipEvent event) {
 
-        // var item = event.getItemStack();
-        // var tip = event.getToolTip();
+        var item = event.getItemStack();
+        var tip = event.getToolTip();
 
-        // if (item.is(zItems.HEMP_SEEDS)) {
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".hemp.spawn"));
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".hemp.grow"));
-        // }
+        if (item.is(zItems.HEMP_SEEDS)) {
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".hemp.spawn"));
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".hemp.grow"));
+        }
 
-        // if (item.is(zItems.SOYBEANS)) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".soy.spawn"));
+        if (item.is(zItems.SOYBEANS)) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".soy.spawn"));
         
-        // if (item.is(zItems.ALOE)) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".aloe.spawn"));
+        if (item.is(zItems.ALOE)) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".aloe.spawn"));
         
-        // if (item.is(zItems.CAVE_WHEAT_SEEDS)) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".cave_wheat.spawn"));
+        if (item.is(zItems.CAVE_WHEAT_SEEDS)) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".cave_wheat.spawn"));
         
-        // if (item.is(zItems.BLUEBERRIES)) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".blueberries.spawn"));
+        if (item.is(zItems.BLUEBERRIES)) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".blueberries.spawn"));
         
-        // if (item.is(zBlocks.LAVENDER.get().asItem())) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".lavender.spawn"));
+        if (item.is(zBlocks.LAVENDER.get().asItem())) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".lavender.spawn"));
         
-        // if (item.is(zBlocks.IRONWOOD_SAPLING.get().asItem())) 
-        //     tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".ironwood.spawn"));
+        if (item.is(zBlocks.IRONWOOD_SAPLING.get().asItem())) 
+            tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".ironwood.spawn"));
         
+
+        if (item.is(zTags.Items.TREE_TAP_HANDLER))
+        {}
+
+        if (item.is(zTags.Items.WINDMILL_REPAIR))
+        {}
+
+        if (item.is(zTags.Items.BEAMS))
+        {}
+        
+
 
         
     }
