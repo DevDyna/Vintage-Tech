7bf292816a8aff854eddf5c5ffa1aee2e6eb0dbb
DevDyna <andrearisorti2003@gmail.com>
2026-08-14 13:09:06 +0200

    Update Main.java

--- src/main/java/com/synergy/vintagetech/Main.java
+++ src/main/java/com/synergy/vintagetech/Main.java
@@ -1,7 +1,10 @@
 package com.synergy.vintagetech;
 
+import com.devdyna.cakesticklib.api.utils.ModAddonUtil;
+import com.devdyna.cakesticklib.api.utils.x;
 import com.synergy.vintagetech.init.Material;
 
+import guideme.Guide;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
@@ -14,6 +17,9 @@ public class Main {
     public Main(IEventBus bus, ModContainer c) {
         Material.register(bus);
         GameEvents.build(bus, c);
+
+        if (ModAddonUtil.checkMod("guideme"))
+            Guide.builder(x.rl(MODULE_ID, "guide")).build();
     }
 
 }
