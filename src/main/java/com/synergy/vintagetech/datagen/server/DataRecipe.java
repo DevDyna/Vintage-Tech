9b66a59ba5e43b889b592139c3e142be209cbd46
DevDyna <andrearisorti2003@gmail.com>
2026-08-18 13:47:54 +0200

    Update DataRecipe.java

--- src/main/java/com/synergy/vintagetech/datagen/server/DataRecipe.java
+++ src/main/java/com/synergy/vintagetech/datagen/server/DataRecipe.java
@@ -5,6 +5,7 @@ import static com.synergy.vintagetech.Main.MODULE_ID;
 import java.util.concurrent.CompletableFuture;
 
 import com.devdyna.cakesticklib.api.datagen.RecipeGenerators;
+import com.devdyna.cakesticklib.setup.common.recipes.hammering.HammeringBuilder;
 import com.devdyna.cakesticklib.setup.registry.LibItems;
 import com.devdyna.cakesticklib.setup.registry.LibTags;
 import com.synergy.vintagetech.init.builder.centrifuge.recipe.CentrifugeBuilder;
@@ -826,34 +827,36 @@ public class DataRecipe extends RecipeProvider implements RecipeGenerators {
                                 .unlockedBy(Items.MILK_BUCKET)
                                 .save(output);
 
-
-
-//TODO API : recipe with scissors
-shapeless(RecipeCategory.MISC, Items.STRING, 3)
-                                .requires(ItemTags.WOOL)
-                                .requires(Items.SHEARS)
-                                .unlockedBy(getHasName(ItemTags.WOOL), has(ItemTags.WOOL))
-                                .save(output, MODULE_ID + ":string_from_wool");
-
-shapeless(RecipeCategory.MISC, Items.STRING, 1)
-                                .requires(ItemTags.WOOL_CARPETS)
-                                .requires(Items.SHEARS)
+                // TODO API : recipe with scissors
+                // TODO API : add fallback to force replace when fail
+                // TODO API : allow x.rl() on .save(output,?) to override suffix
+                //TODO API : add optional boolean to override suffix without use x.rl()
+                HammeringBuilder.of(registries)
+                                .add(ItemTags.WOOL_CARPETS)
+                                .tool(LibItems.HAMMER.get())
                                 .unlockedBy(getHasName(ItemTags.WOOL_CARPETS), has(ItemTags.WOOL_CARPETS))
-                                .save(output, MODULE_ID + ":string_from_wool_carpets");
+                                .output(Items.STRING, 1)
+                                .save(output, "_from_wool_carpets");
 
-                                //TODO API : unlockedBy(TagKey<Item>)
+                HammeringBuilder.of(registries)
+                                .add(ItemTags.WOOL)
+                                .tool(LibItems.HAMMER.get())
+                                .output(Items.STRING, 3)
+                                .unlockedBy(getHasName(ItemTags.WOOL), has(ItemTags.WOOL))
+                                .save(output, "_from_wool");
+
+                // TODO API : unlockedBy(TagKey<Item>)
                 MillstoneBuilder.of(registries)
                                 .input(ItemTags.WOOL_CARPETS)
                                 .output(Items.STRING, 2)
                                 .unlockedBy(getHasName(ItemTags.WOOL_CARPETS), has(ItemTags.WOOL_CARPETS))
-                                .save(output,"_from_wool_carpets");
+                                .save(output, "_from_wool_carpets");
 
                 MillstoneBuilder.of(registries)
                                 .input(ItemTags.WOOL)
                                 .output(Items.STRING, 4)
                                 .unlockedBy(getHasName(ItemTags.WOOL), has(ItemTags.WOOL))
-                                .save(output,"_from_wool");
-
+                                .save(output, "_from_wool");
 
         }
 
